492b2139256de43f3e4cb2830af74421784398cf
mengyuan <15332392019@163.com>
2021-11-07 20:32:40 +0800

    [mengyuan] feat: add useCallback notes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UseRef from './useRef';
+import UseRef from './useRef/useRef';
 import UseContext from './useContext/useContext';
-import UseCallback from './useCallback';
+import UseCallback from './useCallback/useCallback';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/useCallback.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function UseCallback() {
-    return (
-        <div>
-            <input/>
-        </div>
-    )
-}
